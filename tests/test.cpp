91525f1e2ed369485ffed4c1013f056adf5a2890
AlexandrBashkirev <alexandr.bashkirev@gmail.com>
2019-07-29 07:47:12 +0300

    - t

--- tests/test.cpp
+++ tests/test.cpp
@@ -5,13 +5,13 @@ BOOST_AUTO_TEST_SUITE(bulk_test_suite)
 
 BOOST_AUTO_TEST_CASE(test)
 {
-	auto impl = std::make_unique<Implementer>();
+	/*auto impl = std::make_unique<Implementer>();
 	impl->addCmdExecutor(std::move(std::make_unique<CmdExecutor>()));
 
 	CmdStreamWorker worker(std::move(impl));
 
 	std::ifstream ifs("input.log");
-	worker.run(ifs);
+	worker.run(ifs);*/
 }
 
 BOOST_AUTO_TEST_SUITE_END()
